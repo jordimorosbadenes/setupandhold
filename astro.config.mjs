eb14bf4d788ad4186765120b21bb59942f4839a3
jordimorosbadenes <jordimorosbadenes@gmail.com>
2025-12-30 14:32:50 +0100

    Fix relative paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1];
 const isGithub = process.env.GITHUB_ACTIONS === 'true';
 const siteFromEnv = process.env.SITE;
 
-const defaultSite = 'https://setupandhold.github.io';
+const defaultSite = 'https://jordimorosbadenes.github.io/setupandhold';
 
 export default defineConfig({
   site: siteFromEnv || defaultSite,
